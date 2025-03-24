26004a2dcdf4664a0400afc07161dbb73142a04e
leejunnyeop <daffodil8620@naver.com>
2025-03-24 18:55:10 +0900

    [feat] : 사용자 로그인 구현

--- src/main/java/com/codit/blog/service/user/UerServiceImpl.java
+++ src/main/java/com/codit/blog/service/user/UerServiceImpl.java
@@ -1,8 +1,11 @@
-package com.codit.blog.service;
+package com.codit.blog.service.user;
 
-import com.codit.blog.domain.dto.UserRequestDto;
+import com.codit.blog.domain.dto.userDto.UserLoginRequestDto;
+import com.codit.blog.domain.dto.userDto.UserLoginResponse;
+import com.codit.blog.domain.dto.userDto.UserRequestDto;
 import com.codit.blog.domain.entity.User;
 import com.codit.blog.domain.mapper.UserMapper;
+import com.codit.blog.jwt.JwtUtil;
 import com.codit.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
@@ -10,9 +13,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UerServiceImpl implements UserService{
+public class UerServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private static Boolean check = Boolean.TRUE;
 
 
     @Override
@@ -31,8 +36,16 @@ public class UerServiceImpl implements UserService{
     }
 
     @Override
-    public void login(UserRequestDto userRequestDto) {
+    public UserLoginResponse login(UserLoginRequestDto userLoginRequestDto) {
+        User user = userRepository.findById(userLoginRequestDto.uuid())
+                .orElseThrow(() -> new IllegalArgumentException("없는 회원 입니다. 확인 부탁드립니다"));
 
+        if(!BCrypt.checkpw(userLoginRequestDto.password(), user.getPassword())){
+            check = false;
+            return UserMapper.toUserLoginResponse(check, null);
+        }
+        String generateToken = jwtUtil.generateToken(user.getId().toString());
+        return UserMapper.toUserLoginResponse(check, generateToken);
     }
 
     @Override
